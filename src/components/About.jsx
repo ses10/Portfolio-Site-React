b5e9f27440753f632d472a859dc8b066d271de38
ses10 <sesma@live.com>
2019-07-21 10:52:07 -0400

    remove p variant

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About(){
         <div id="about">
             <Box paddingLeft={7} paddingRight={7} paddingTop={5} paddingBottom={5}>
                 <Box marginBottom={1}><Typography variant="h5">About Me</Typography></Box>
-                <Typography variant="p" lineHeight={5} >Experienced backend developer specializing in the Java/Spring tech stack. I've integrated systems using RESTful web services and implemented 
+                <Typography>Experienced backend developer specializing in the Java/Spring tech stack. I've integrated systems using RESTful web services and implemented 
 a microservices based architecture for ETL processing. Light front end experience with Javascript/React. I am interested in distributed systems and 
 building scalable web applications impacting millions of users. My career goal to be become a software architect.</Typography> 
             </Box>
